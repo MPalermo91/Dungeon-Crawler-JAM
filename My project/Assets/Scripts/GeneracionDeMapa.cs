e2bc7469cf48126b150806351a3f13c49d9f6b1e
NLahorcaKatzow <nicolas.lahorca.k@gmail.com>
2023-02-20 23:13:45 -0300

    Commit inicial ~LNK

---
+++ My project/Assets/Scripts/GeneracionDeMapa.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneracionDeMapa : MonoBehaviour
+{
+    public class Cell
+    {
+        public bool visited = false;
+        public bool[] status = new bool[4];
+    }
+
+    public Vector2 size;
+    public int startPos = 0;
+
+    List<Cell> board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void MazeGenerator()
+    {
+        board = new List<Cell>();
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                board.Add(new Cell());
+            }
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+
+        int k = 0;
+
+        while(k<1000)
+        {
+            k++;
+
+            board[currentCell].visited = true;
+
+            //check the cell´s neightbors
+            List<int> neighbors = CheckNeighbors(currentCell);
+
+            if(neighbors.Count == 0)
+            {
+                if(path.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    currentCell = path.Pop();
+                }
+            }
+            else
+            {
+                path.Push(currentCell);
+
+                int newCell = neighbors[Random.Range(0, neighbors.Count)];
+
+                if (newCell> currentCell)
+                {
+                    
+                    //down or right
+                    if (newCell -1 == currentCell)
+                    {
+                        board[currentCell].status[2] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[3] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[1] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[0] = true;
+                    }
+                }
+                else
+                {
+                    //up or left
+                    if (newCell + 1 == currentCell)
+                    {
+                        board[currentCell].status[3] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[2] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[0] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[1] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    List<int> CheckNeighbors(int cell)
+    {
+        List<int> neighbors = new List<int>();
+
+        //check up
+        if(cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell - size.x));
+        }
+        //check down
+        if(cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + size.x));
+        }
+        //check right
+        if((cell+1) % size.x != 0 && !board[Mathf.FloorToInt(cell + 1)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + 1));
+        }
+        //check left
+        if(cell % size.x != 0 && !board[Mathf.FloorToInt(cell - 1)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell - 1));
+        }
+
+
+        return neighbors;
+    }
+}
