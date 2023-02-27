cb9890b61d9f19f7215d487c65f50069880ca594
NLahorcaKatzow <nicolas.lahorca.k@gmail.com>
2023-02-26 22:56:00 -0300

    26/02 Modif. Generacion de mapa
    Modificacion de generacion de mapa.

--- My project/Assets/Scripts/GeneracionDeMapa.cs
+++ My project/Assets/Scripts/GeneracionDeMapa.cs
@@ -4,163 +4,285 @@ using UnityEngine;
 
 public class GeneracionDeMapa : MonoBehaviour
 {
-    public class Cell
+    
+    [SerializeField] MazeNode nodePrefab;
+    [SerializeField] Vector2Int mazeSize;
+    [SerializeField] GameObject zonaBoss;
+    [SerializeField] GameObject zonaSpawn;
+
+    [SerializeField] float spawnCofresmax = 10;
+    [SerializeField] float spawnEnemigosMax = 40;
+    [SerializeField] GameObject cofre;
+    [SerializeField] GameObject enemigoP1;
+
+    //[SerializeField] float nodeSize;
+
+    
+    private void GeneradorDeEntidades(Vector3 pos)
     {
-        public bool visited = false;
-        public bool[] status = new bool[4];
-    }
+        int num = Random.Range(0, 101);
 
-    public Vector2 size;
-    public int startPos = 0;
-    public GameObject room;
-    public Vector2 offset;
-
-    List<Cell> board;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        MazeGenerator();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void GenerateDungeon()
-    {
-        for (int i = 0; i < size.x; i++)
+        if (num < spawnCofresmax)
         {
-            for (int j = 0; j < size.y; j++)
+            Instantiate(cofre, pos, Quaternion.identity, transform);
+        }
+        else if (num > spawnCofresmax && num < (spawnCofresmax + spawnEnemigosMax))
+        {
+            Instantiate(enemigoP1, pos, Quaternion.identity, transform);
+        }
+    }
+
+
+
+    private void Start()
+    {
+        GenerateMazeInstant(mazeSize);
+        //StartCoroutine(GenerateMaze(mazeSize));
+    }
+
+    void GenerateMazeInstant(Vector2Int size)
+    {
+        List<MazeNode> nodes = new List<MazeNode>();
+
+        // Create nodes
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
             {
-                Cell currentCell = board[Mathf.FloorToInt(i+j*size.x)];
-                if (currentCell.visited)
+                Vector3 nodePos = new Vector3(x - (size.x / 2f), 0, y - (size.y / 2f));
+                MazeNode newNode = Instantiate(nodePrefab, nodePos, Quaternion.identity, transform);
+                nodes.Add(newNode);
+                if (x == 0 && y == 0)
                 {
-                    var newRoom = Instantiate(room, new Vector3(i*offset.x, 0, -j*offset.y), Quaternion.identity, transform).GetComponent<Habitaciones>();
-                    newRoom.UpdateRoom(currentCell.status);
+                    Instantiate(zonaSpawn, nodePos, Quaternion.identity, transform);
+                    //este es el primer nodo
+                }
+                if (x == size.x - 1 && y == size.y - 1)
+                {
+                    Instantiate(zonaBoss, nodePos, Quaternion.identity, transform);
+                }
+                if (x != 0 && y != 0 && x!= size.x -1 && y != size.y - 1)
+                {
+                    GeneradorDeEntidades(nodePos);
+                }
                 
-                    newRoom.name += " " + i + "-" + j;  
-                }
-            }
-        }
-    }
-
-
-    void MazeGenerator()
-    {
-        board = new List<Cell>();
-
-        for (int i = 0; i < size.x; i++)
-        {
-            for (int j = 0; j < size.y; j++)
-            {
-                board.Add(new Cell());
             }
         }
 
-        int currentCell = startPos;
+        List<MazeNode> currentPath = new List<MazeNode>();
+        List<MazeNode> completedNodes = new List<MazeNode>();
 
-        Stack<int> path = new Stack<int>();
+        // Choose starting node
+        currentPath.Add(nodes[Random.Range(0, nodes.Count)]);
+        currentPath[0].SetState(NodeState.Current);
+        
 
-        int k = 0;
-
-        while(k<1000)
+        while (completedNodes.Count < nodes.Count)
         {
-            k++;
+            // Check nodes next to the current node
+            List<int> possibleNextNodes = new List<int>();
+            List<int> possibleDirections = new List<int>();
 
-            board[currentCell].visited = true;
+            int currentNodeIndex = nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            int currentNodeX = currentNodeIndex / size.y;
+            int currentNodeY = currentNodeIndex % size.y;
 
-            if(currentCell == board.Count - 1)
+            if (currentNodeX < size.x - 1)
             {
-                break;
+                // Check node to the right of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + size.y]))
+                {
+                    possibleDirections.Add(1);
+                    possibleNextNodes.Add(currentNodeIndex + size.y);
+                }
+            }
+            if (currentNodeX > 0)
+            {
+                // Check node to the left of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - size.y]))
+                {
+                    possibleDirections.Add(2);
+                    possibleNextNodes.Add(currentNodeIndex - size.y);
+                }
+            }
+            if (currentNodeY < size.y - 1)
+            {
+                // Check node above the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + 1]))
+                {
+                    possibleDirections.Add(3);
+                    possibleNextNodes.Add(currentNodeIndex + 1);
+                }
+            }
+            if (currentNodeY > 0)
+            {
+                // Check node below the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - 1]))
+                {
+                    possibleDirections.Add(4);
+                    possibleNextNodes.Add(currentNodeIndex - 1);
+                }
             }
 
-            //check the cell´s neightbors
-            List<int> neighbors = CheckNeighbors(currentCell);
-
-            if(neighbors.Count == 0)
+            // Choose next node
+            if (possibleDirections.Count > 0)
             {
-                if(path.Count == 0)
+                int chosenDirection = Random.Range(0, possibleDirections.Count);
+                MazeNode chosenNode = nodes[possibleNextNodes[chosenDirection]];
+
+                switch (possibleDirections[chosenDirection])
                 {
-                    break;
+                    case 1:
+                        chosenNode.RemoveWall(1);
+                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        break;
+                    case 2:
+                        chosenNode.RemoveWall(0);
+                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        break;
+                    case 3:
+                        chosenNode.RemoveWall(3);
+                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        break;
+                    case 4:
+                        chosenNode.RemoveWall(2);
+                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        break;
                 }
-                else
-                {
-                    currentCell = path.Pop();
-                }
+
+                currentPath.Add(chosenNode);
+                chosenNode.SetState(NodeState.Current);
             }
             else
             {
-                path.Push(currentCell);
+                completedNodes.Add(currentPath[currentPath.Count - 1]);
 
-                int newCell = neighbors[Random.Range(0, neighbors.Count)];
+                currentPath[currentPath.Count - 1].SetState(NodeState.Completed);
+                currentPath.RemoveAt(currentPath.Count - 1);
 
-                if (newCell> currentCell)
-                {
-                    
-                    //down or right
-                    if (newCell -1 == currentCell)
-                    {
-                        board[currentCell].status[2] = true;
-                        currentCell = newCell;
-                        board[currentCell].status[3] = true;
-                    }
-                    else
-                    {
-                        board[currentCell].status[1] = true;
-                        currentCell = newCell;
-                        board[currentCell].status[0] = true;
-                    }
-                }
-                else
-                {
-                    //up or left
-                    if (newCell + 1 == currentCell)
-                    {
-                        board[currentCell].status[3] = true;
-                        currentCell = newCell;
-                        board[currentCell].status[2] = true;
-                    }
-                    else
-                    {
-                        board[currentCell].status[0] = true;
-                        currentCell = newCell;
-                        board[currentCell].status[1] = true;
-                    }
-                }
+                //Instantiate(cofre, nodePos, Quaternion.identity, transform);
             }
         }
-        GenerateDungeon();
     }
 
-    List<int> CheckNeighbors(int cell)
+    IEnumerator GenerateMaze(Vector2Int size)
     {
-        List<int> neighbors = new List<int>();
+        List<MazeNode> nodes = new List<MazeNode>();
 
-        //check up
-        if(cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited)
+        // Create nodes
+        for (int x = 0; x < size.x; x++)
         {
-            neighbors.Add(Mathf.FloorToInt(cell - size.x));
-        }
-        //check down
-        if(cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited)
-        {
-            neighbors.Add(Mathf.FloorToInt(cell + size.x));
-        }
-        //check right
-        if((cell+1) % size.x != 0 && !board[Mathf.FloorToInt(cell + 1)].visited)
-        {
-            neighbors.Add(Mathf.FloorToInt(cell + 1));
-        }
-        //check left
-        if(cell % size.x != 0 && !board[Mathf.FloorToInt(cell - 1)].visited)
-        {
-            neighbors.Add(Mathf.FloorToInt(cell - 1));
+            for (int y = 0; y < size.y; y++)
+            {
+                Vector3 nodePos = new Vector3(x - (size.x / 2f), 0, y - (size.y / 2f));
+                MazeNode newNode = Instantiate(nodePrefab, nodePos, Quaternion.identity, transform);
+                nodes.Add(newNode);
+
+                yield return null;
+            }
         }
 
+        List<MazeNode> currentPath = new List<MazeNode>();
+        List<MazeNode> completedNodes = new List<MazeNode>();
 
-        return neighbors;
+        // Choose starting node
+        currentPath.Add(nodes[Random.Range(0, nodes.Count)]);
+        currentPath[0].SetState(NodeState.Current);
+
+        while (completedNodes.Count < nodes.Count)
+        {
+            // Check nodes next to the current node
+            List<int> possibleNextNodes = new List<int>();
+            List<int> possibleDirections = new List<int>();
+
+            int currentNodeIndex = nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            int currentNodeX = currentNodeIndex / size.y;
+            int currentNodeY = currentNodeIndex % size.y;
+
+            if (currentNodeX < size.x - 1)
+            {
+                // Check node to the right of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + size.y]) && 
+                    !currentPath.Contains(nodes[currentNodeIndex + size.y]))
+                {
+                    possibleDirections.Add(1);
+                    possibleNextNodes.Add(currentNodeIndex + size.y);
+                }
+            }
+            if (currentNodeX > 0)
+            {
+                // Check node to the left of the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - size.y]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - size.y]))
+                {
+                    possibleDirections.Add(2);
+                    possibleNextNodes.Add(currentNodeIndex - size.y);
+                }
+            }
+            if (currentNodeY < size.y - 1)
+            {
+                // Check node above the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex + 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex + 1]))
+                {
+                    possibleDirections.Add(3);
+                    possibleNextNodes.Add(currentNodeIndex + 1);
+                }
+            }
+            if (currentNodeY > 0)
+            {
+                // Check node below the current node
+                if (!completedNodes.Contains(nodes[currentNodeIndex - 1]) &&
+                    !currentPath.Contains(nodes[currentNodeIndex - 1]))
+                {
+                    possibleDirections.Add(4);
+                    possibleNextNodes.Add(currentNodeIndex - 1);
+                }
+            }
+
+            // Choose next node
+            if (possibleDirections.Count > 0)
+            {
+                int chosenDirection = Random.Range(0, possibleDirections.Count);
+                MazeNode chosenNode = nodes[possibleNextNodes[chosenDirection]];
+
+                switch (possibleDirections[chosenDirection])
+                {
+                    case 1:
+                        chosenNode.RemoveWall(1);
+                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        break;
+                    case 2:
+                        chosenNode.RemoveWall(0);
+                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        break;
+                    case 3:
+                        chosenNode.RemoveWall(3);
+                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        break;
+                    case 4:
+                        chosenNode.RemoveWall(2);
+                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        break;
+                }
+
+                currentPath.Add(chosenNode);
+                chosenNode.SetState(NodeState.Current);
+            }
+            else
+            {
+                completedNodes.Add(currentPath[currentPath.Count - 1]);
+
+                currentPath[currentPath.Count - 1].SetState(NodeState.Completed);
+                currentPath.RemoveAt(currentPath.Count - 1);
+                //este es la habitacion final
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
